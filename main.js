edfc1065d4b142045e8836d56b4e4c6357089020
Adrian-ortiz0 <dxniel7328@gmail.com>
2024-08-26 16:37:27 -0500

    feat: :sparkles: avance del carrito

--- main.js
+++ main.js
@@ -38,19 +38,46 @@ async function trayendoImagenes() {
   const contenedor = document.getElementById("store__container");
   imagenes.forEach((imagen) => {
     const subContenedor = document.createElement("div");
+    subContenedor.classList.add("productos__store");
     subContenedor.id = "0" + imagen.id;
     subContenedor.innerHTML = `<img src=${imagen.imagen} alt="t1" />
         <button class="botonsitos">Agregar al carrito</button>`;
     contenedor.append(subContenedor);
     const boton = subContenedor.querySelector(".botonsitos");
     boton.addEventListener("click", function () {
-      agregarAlCarrito();
+      agregarAlCarrito(imagen.id);
     });
   });
 }
 
-async function agregarAlCarrito() {
-  console.log("añadido");
+async function agregarAlCarrito(productoId) {
+  const imagenes = await peticion(jsonImagenes);
+  const producto = imagenes.find(function (item) {
+    return item.id === productoId;
+  });
+  if (producto) {
+    carrito.push(producto);
+    console.log(`Producto ${producto.nombre} agregado al carrito`);
+    console.log(carrito);
+    const contenedorCarrito = document.getElementById("carrito");
+    const divContainerProducto = document.createElement("div");
+    divContainerProducto.classList.add("plantillaCarrito");
+    divContainerProducto.innerHTML = `<img src=${producto.imagen} alt="t1" width="150px" />
+        <div class="opcionesCarrito">
+          <h2>${producto.nombre}</h2>
+          <p>1</p>
+          <h2>${producto.precio}$</h2>
+          <div class="contenedorBotonesCarrito">
+            <button class="botonesCarrito" id="btnPlus">+</button>
+            <button class="botonesCarrito" id="btnMinus">-</button>
+            <button class="botonesCarrito" id="buyBtn">Buy</button>
+            <button class="botonesCarrito" id="deleteBtn">Delete</button>
+          </div>
+        </div>`;
+    contenedorCarrito.append(divContainerProducto);
+  } else {
+    console.log("Producto no encontrado");
+  }
 }
 
 function abrirCarrito() {
